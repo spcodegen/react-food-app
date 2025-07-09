a5c1b394f7e82bbb369f07bca3b2778bd985d5aa
spcodegen <shehansp2000@gmail.com>
2025-07-09 09:39:44 +0530

    Creating State To Save Food Data

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import Search from './components/Search'
 
 function App() {
-  
-
+  const [foodData,setFoodData] = useState([]);
   return (
     <div className="App">
-      <Search/>
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      {foodData.map((food)=>(<h2 key={food.id}>{food.title}</h2>))}
     </div>
   )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,13 +3,14 @@ import React, { useEffect, useState } from 'react'
 const URL = "https://api.spoonacular.com/recipes/complexSearch"
 const API_KEY = "f9f1a906a20d409d8f0c08ca3625de17"
 
-function Search() {
+function Search({foodData,setFoodData}) {
   const [query,setQuery] = useState("");  
   useEffect(()=>{
     async function fetchFood() {
         const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
         const data = await res.json();
         console.log(data.results);
+        setFoodData(data.results);
     }
     fetchFood();
   },[query])
