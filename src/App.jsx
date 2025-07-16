455748ef1ada81b576f3a9a49cd1b5ca04ba932d
spcodegen <shehansp2000@gmail.com>
2025-07-16 12:02:09 +0530

    Outer Container Component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Search from "./components/Search";
 import FoodList from "./components/FoodList";
 import Nav from "./components/Nav";
 import "./App.css";
+import Container from "./components/Container";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
@@ -10,7 +11,9 @@ function App() {
     <div className="App">
       <Nav />
       <Search foodData={foodData} setFoodData={setFoodData} />
-      <FoodList foodData={foodData} />
+      <Container>
+        <FoodList foodData={foodData} />
+      </Container>
     </div>
   );
 }
---
+++ src/components/Container.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./container.module.css";
+
+function Container({ children }) {
+  return <div className={styles.parentContainer}>{children}</div>;
+}
+
+export default Container;
