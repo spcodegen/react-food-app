939d2293057370aba45f6eda49a3cdb04add1e1b
spcodegen <shehansp2000@gmail.com>
2025-07-17 11:50:39 +0530

    Fetching Recipes From API

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function FoodDetails({ foodId }) {
-  return <div>Food Details {foodId}</div>;
+  const [food, setFood] = useState("");
+  const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
+  const API_KEY = "f9f1a906a20d409d8f0c08ca3625de17";
+
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data);
+      setFood(data);
+    }
+    fetchFood();
+  }, [foodId]);
+
+  return (
+    <div>
+      Food Details {foodId} {food.title} <img src={food.image} alt="" />
+    </div>
+  );
 }
 
 export default FoodDetails;
