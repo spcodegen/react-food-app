c965403a8297a5e95223c710a3c3f5c4f9aba3f9
spcodegen <shehansp2000@gmail.com>
2025-07-22 15:10:21 +0530

    Styling Items Container

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,17 +1,24 @@
 import React from "react";
+import styles from "./item.module.css";
 
 function Item({ item }) {
   return (
     <div>
-      <div>
-        <img
-          src={`https://img.spoonacular.com/ingredients_100x100/` + item.image}
-          alt={item.name}
-        />
-        <h3>{item.name}</h3>
-        <h3>
-          {item.amount} {item.unit}
-        </h3>
+      <div className={styles.itemContainer}>
+        <div className={styles.imageContainer}>
+          <img
+            src={
+              `https://img.spoonacular.com/ingredients_100x100/` + item.image
+            }
+            alt={item.name}
+          />
+        </div>
+        <div className={styles.nameContainer}>
+          <div className={styles.name}>{item.name}</div>
+          <div className={styles.amount}>
+            {item.amount} {item.unit}
+          </div>
+        </div>
       </div>
     </div>
   );
